7e902c26007f20803948057674636b2a0d0c0cab
jagan naidu <janaidu01@gmail.com>
2017-11-12 19:53:22 +0000

    first commit

--- source-code/allaboutscala/src/main/scala/com/allaboutscala/chapter/one/tutorial_04/HelloWorld.scala
+++ source-code/allaboutscala/src/main/scala/com/allaboutscala/chapter/one/tutorial_04/HelloWorld.scala
@@ -24,6 +24,10 @@ package com.allaboutscala.chapter.one.tutorial_04
   */
 object HelloWorld extends App {
 
+  /// this is the test project git test
+
+
+
   println("Hello World from Scala!")
 
 }
